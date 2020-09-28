7221cfbde9b1625cf444059618e9145993b00d7d
Zetta Zone <zettazone24@gmail.com>
2020-09-28 18:13:22 +0300

    first commit

---
+++ build/precache-manifest.e2c9403215a9919b2dd4f6113df18908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5740badf4fa2dc133f862970ac83eeb0",
+    "url": "/React-Template/index.html"
+  },
+  {
+    "revision": "9d648a2f716042d6c492",
+    "url": "/React-Template/static/css/main.5e035f44.chunk.css"
+  },
+  {
+    "revision": "4f293b81beaa9fc6dede",
+    "url": "/React-Template/static/js/2.7b5cc6e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Template/static/js/2.7b5cc6e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d648a2f716042d6c492",
+    "url": "/React-Template/static/js/main.b86a2cf7.chunk.js"
+  },
+  {
+    "revision": "dc4c393f115770d105c6",
+    "url": "/React-Template/static/js/runtime-main.9d9b1f7a.js"
+  }
+]);
